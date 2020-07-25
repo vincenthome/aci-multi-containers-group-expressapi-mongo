69ea090920ec1c17848b067fc4f8f205fdfdabfb
vincenthome <vincenthome@hotmail.com>
2020-07-25 11:29:19 -0400

    update environment variables

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:12.18-alpine
+ENV NODE_ENV production
+ENV HELLO hello-dockefile
+WORKDIR /usr/src/app
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+RUN npm install --production --silent && mv node_modules ../
+COPY . .
+# EXPOSE 80 # NOT BEING USED AT runtime, ALWAYS override by CLI -p 
+CMD npm start
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
+const hello = process.env.HELLO || 'notfound-index.js';
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
+  console.log(`Environment HELLO - ${hello}`);
   /* eslint-enable no-console */
 });
